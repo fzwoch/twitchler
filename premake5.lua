06a145c24b576e5aec6dee22e81bf02391e50a0f
Florian Zwoch <fzwoch@gmail.com>
2015-10-10 09:49:07 +0200

    windows 64 bit, official sdk.. msys2

--- premake5.lua
+++ premake5.lua
@@ -27,57 +27,25 @@ solution "Twitchler"
 		defines "NDEBUG"
 		optimize "full"
 
-	filter "system:windows"
-		toolset "msc"
-		architecture "x86"
-		flags "unicode"
-
 	filter "system:macosx"
 		toolset "clang"
-		architecture "x86_64"
 
 	project "twitchler"
 		kind "WindowedApp"
 		language "C++"
-		files { "src/**.cpp", "include/**.h" }
+		files { "src/*.cpp", "include/*.h" }
 		includedirs "include"
 
-		local wx_config_cflags
-		local wx_config_libs
-		local pkg_config_cflags
-		local pkg_config_libs
+		filter "system:macosx"
+			buildoptions { "`wx-config --cflags`", "`PKG_CONFIG_DIR=/Library/Frameworks/GStreamer.framework/Libraries/pkgconfig/ pkg-config --cflags gstreamer-1.0 gstreamer-video-1.0 libsoup-2.4 json-glib-1.0`", "-x objective-c++" }
+			linkoptions { "`wx-config --libs`", "`PKG_CONFIG_DIR=/Library/Frameworks/GStreamer.framework/Libraries/pkgconfig/ pkg-config --libs gstreamer-1.0 gstreamer-video-1.0 libsoup-2.4 json-glib-1.0`", "-headerpad_max_install_names" }
 
-		if (os.get() == "macosx") then
-			wx_config_cflags = os.outputof("wx-config --cflags")
-			wx_config_libs = os.outputof("wx-config --libs")
-
-			pkg_config_cflags = os.outputof("PKG_CONFIG_DIR=/Library/Frameworks/GStreamer.framework/Libraries/pkgconfig/ pkg-config --cflags gstreamer-1.0 gstreamer-video-1.0 libsoup-2.4 json-glib-1.0")
-			pkg_config_libs = os.outputof("PKG_CONFIG_DIR=/Library/Frameworks/GStreamer.framework/Libraries/pkgconfig/pkg-config --libs gstreamer-1.0 gstreamer-video-1.0 libsoup-2.4 json-glib-1.0")
-		end
-
-		if (os.get() == "linux") then
-			wx_config_cflags = os.outputof("wx-config --cflags")
-			wx_config_libs = os.outputof("wx-config --libs")
-
-			pkg_config_cflags = os.outputof("pkg-config --cflags gstreamer-1.0 gstreamer-video-1.0 libsoup-2.4 json-glib-1.0 gtk+-2.0")
-			pkg_config_libs = os.outputof("pkg-config --libs gstreamer-1.0 gstreamer-video-1.0 libsoup-2.4 json-glib-1.0 gtk+-2.0")
-		end
+		filter "system:linux"
+			buildoptions { "`wx-config --cflags`", "`pkg-config --cflags gstreamer-1.0 gstreamer-video-1.0 libsoup-2.4 json-glib-1.0 gtk+-2.0`" }
+			linkoptions { "`wx-config --libs`", "`pkg-config --libs gstreamer-1.0 gstreamer-video-1.0 libsoup-2.4 json-glib-1.0 gtk+-2.0`" }
 
 		filter "system:windows"
 			files "*.rc"
-			defines { "WXUSINGDLL", "wxMSVC_VERSION_AUTO", "_CRT_SECURE_NO_WARNINGS", "WXMAKINGDLL_JSON", "WXMAKINGDLL_WXCURL" }
-			includedirs { "C:/wxWidgets/include/msvc", "C:/wxWidgets/include" }
-			includedirs { "C:/GStreamer/1.0/x86/include/gstreamer-1.0", "C:/GStreamer/1.0/x86/include/glib-2.0" }
-			includedirs { "C:/GStreamer/1.0/x86/lib/gstreamer-1.0/include", "C:/GStreamer/1.0/x86/lib/glib-2.0/include" }
-			includedirs { "C:/curl/include" }
-			libdirs { "C:/wxWidgets/lib/vc120_dll", "C:/GStreamer/1.0/x86/lib", "C:/curl/lib" }
-			links { "gmodule-2.0.lib", "gio-2.0.lib", "gstvideo-1.0.lib", "glib-2.0.lib", "gstreamer-1.0.lib", "gobject-2.0.lib" }
-			links { "libcurldll.lib" }
-
-		filter "system:macosx"
-			buildoptions { (wx_config_cflags), (pkg_config_cflags), "-x objective-c++" }
-			linkoptions { (wx_config_libs), (pkg_config_libs), "-headerpad_max_install_names" }
-
-		filter "system:linux"
-			buildoptions { (wx_config_cflags), (pkg_config_cflags) }
-			linkoptions { (wx_config_libs), (pkg_config_libs) }
+			resincludedirs "/mingw64/include/wx-3.0"
+			buildoptions { "`wx-config --cflags`", "`PKG_CONFIG_PATH=/c/gstreamer/1.0/x86_64/lib/pkgconfig pkg-config --cflags gstreamer-1.0 gstreamer-video-1.0 libsoup-2.4 json-glib-1.0`" }
+			linkoptions { "`wx-config --libs`", "`PKG_CONFIG_PATH=/c/gstreamer/1.0/x86_64/lib/pkgconfig pkg-config --libs gstreamer-1.0 gstreamer-video-1.0 libsoup-2.4 json-glib-1.0`", "-static-libgcc", "-static-libstdc++" }
